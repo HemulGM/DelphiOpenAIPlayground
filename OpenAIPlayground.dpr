8d0ee7b58a765ff6c8fcfbb043c3fc29d878bd63
HemulGM <alinvip22@gmail.com>
2023-01-13 18:57:06 +0500

    Publish repo

--- OpenAIPlayground.dpr
+++ OpenAIPlayground.dpr
@@ -5,7 +5,20 @@ uses
   FMX.Forms,
   ChatGPT.Main in 'ChatGPT.Main.pas' {FormMain},
   ChatGPT.Frame.Code in 'ChatGPT.Frame.Code.pas' {FrameCode: TFrame},
-  ChatGPT.Frame.Score in 'ChatGPT.Frame.Score.pas' {FrameScore: TFrame};
+  ChatGPT.Frame.Score in 'ChatGPT.Frame.Score.pas' {FrameScore: TFrame},
+  OpenAI.API.Params in 'DelphiOpenAI\OpenAI.API.Params.pas',
+  OpenAI.API in 'DelphiOpenAI\OpenAI.API.pas',
+  OpenAI.Completions in 'DelphiOpenAI\OpenAI.Completions.pas',
+  OpenAI.Edits in 'DelphiOpenAI\OpenAI.Edits.pas',
+  OpenAI.Embeddings in 'DelphiOpenAI\OpenAI.Embeddings.pas',
+  OpenAI.Engines in 'DelphiOpenAI\OpenAI.Engines.pas',
+  OpenAI.Errors in 'DelphiOpenAI\OpenAI.Errors.pas',
+  OpenAI.Files in 'DelphiOpenAI\OpenAI.Files.pas',
+  OpenAI.FineTunes in 'DelphiOpenAI\OpenAI.FineTunes.pas',
+  OpenAI.Images in 'DelphiOpenAI\OpenAI.Images.pas',
+  OpenAI.Models in 'DelphiOpenAI\OpenAI.Models.pas',
+  OpenAI.Moderations in 'DelphiOpenAI\OpenAI.Moderations.pas',
+  OpenAI in 'DelphiOpenAI\OpenAI.pas';
 
 {$R *.res}
 
